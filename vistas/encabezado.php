7414895c2572d13b7ad7b261f909f10dd1e87a3e
esteban1234 <tva_libra@hotmail.com>
2018-06-13 17:02:03 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="6dx6QslSDD1GLYqtLyLpmoSuo-QY05_1-1D9i19ON1I" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>COORPORATIVO SOLISUR</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
